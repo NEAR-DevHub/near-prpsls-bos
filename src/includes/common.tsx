3f91416ebab61e81b036d2ad7a14db5a86f7d8fe
Megha-Dev-19 <meghadev1901@gmail.com>
2024-04-30 23:04:58 +0530

    minor updates

--- src/includes/common.tsx
+++ src/includes/common.tsx
@@ -1,4 +1,4 @@
 export const REPL_DEVHUB = "devhub.near";
 export const REPL_INFRASTRUCTURE_COMMITTEE = "infrastructure-committee.near";
-export const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT = "devhub.near";
+export const REPL_INFRASTRUCTURE_COMMITTEE_CONTRACT = "truedove38.near";
 export const REPL_RPC_URL = "https://rpc.mainnet.near.org";
