3c3ea03949243a89f1e90199befc4f04c10532f5
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2024-05-13 13:44:22 +0530

    UI updates

--- src/components/molecule/FilterByLabel.jsx
+++ src/components/molecule/FilterByLabel.jsx
@@ -1,8 +1,10 @@
 import { REPL_DEVHUB } from "@/includes/common";
 const availableOptions = props.availableOptions;
-const options = (availableOptions ?? []).map((i) => {
-  return { label: i.title, value: i.value };
-});
+const options =
+  (availableOptions ?? []).map((i) => {
+    return { label: i.title, value: i.value };
+  }) ?? [];
+options.push({ label: "None", value: null });
 const setSelected = props.onStateChange ?? (() => {});
 
 return (
