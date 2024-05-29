808771ae3b3ce93430f543deb917da7020c1442c
Peter Salomonsen <petersalomonsen@runbox.com>
2024-05-29 11:38:25 +0200

    Tests for editing RFP (#12)
    * edit RFP test
    
    * wip: edit RFP with don't ask again
    
    * wait for transaction to be finished
    
    * mock new RFP content after transaction submit, check the viewed content after coming back to view mode. combination of multiple routes in playwright test using fallback.

--- playwright-tests/tests/rfp.spec.js
+++ playwright-tests/tests/rfp.spec.js
@@ -1,6 +1,11 @@
 import { test, expect } from "@playwright/test";
 import { pauseIfVideoRecording } from "../util/videorecording";
 import { mockRpcRequest } from "../util/rpcmock";
+import {
+  setupRPCResponsesForDontAskAgain,
+  transactionCompleted,
+} from "../util/dontaskagain";
+import { setDontAskAgainCacheValues } from "../util/cache";
 
 test.describe("Wallet is connected", () => {
   test.use({
@@ -163,6 +168,128 @@ test.describe("Wallet is connected with admin account", () => {
         1
       )
     );
+    await pauseIfVideoRecording(page);
+  });
+  test("should edit RFP", async ({ page }) => {
+    await mockRpcRequest({
+      page,
+      filterParams: {
+        method_name: "get_rfp",
+      },
+      modifyOriginalResultFunction: async (originalResult) => {
+        console.log(JSON.stringify(originalResult, null, 1));
+        originalResult.snapshot.timeline.status = "ACCEPTING_SUBMISSIONS";
+        originalResult.snapshot.linked_proposals = [2, 3];
+        return originalResult;
+      },
+    });
+
+    await page.goto(
+      "/infrastructure-committee.near/widget/near-prpsls-bos.components.pages.app?page=rfp&id=1"
+    );
+    await page.getByRole("button", { name: "Edit" }).click();
+
+    await page.locator(".badge .bi-trash3-fill").click();
+    await page.getByText("Select Category").click();
+    await page.getByText("Explorers").click();
+
+    await expect(page.locator(".badge")).toHaveText("Explorers");
+    await page
+      .locator('input[type="text"]')
+      .pressSequentially("test edited title");
+    await page.locator('input[type="date"]').pressSequentially("01052030");
+    await page
+      .locator('textarea[type="text"]')
+      .pressSequentially("the edited rfp summary");
+    await page
+      .frameLocator("iframe")
+      .locator(".CodeMirror textarea")
+      .pressSequentially("The edited RFP description");
+
+    await pauseIfVideoRecording(page);
+    await page.getByRole("button", { name: "Submit" }).click();
+
+    const transactionText = JSON.stringify(
+      JSON.parse(await page.locator("div.modal-body code").innerText()),
+      null,
+      1
+    );
+    await expect(transactionText).toEqual(
+      JSON.stringify(
+        {
+          labels: ["Explorers"],
+          body: {
+            rfp_body_version: "V0",
+            name: "test edited titletest",
+            description: "test",
+            summary: "the edited rfp summarytest",
+            submission_deadline: "1893801600000000000",
+            timeline: {
+              status: "ACCEPTING_SUBMISSIONS",
+            },
+          },
+          id: 1,
+        },
+        null,
+        1
+      )
+    );
+  });
+});
+
+test.describe("Admin with don't ask again enabled", () => {
+  test.use({
+    storageState:
+      "playwright-tests/storage-states/wallet-connected-admin-dont-ask-again.json",
+  });
+  test("should edit RFP", async ({ page }) => {
+    const theNewDescription = "The edited RFP description";
+    await setupRPCResponsesForDontAskAgain(page);
+    await mockRpcRequest({
+      page,
+      filterParams: {
+        method_name: "get_rfp",
+      },
+      modifyOriginalResultFunction: async (originalResult) => {
+        if (transactionCompleted) {
+          originalResult.snapshot.description = theNewDescription;
+          originalResult.snapshot.timestamp = (
+            BigInt(new Date().getTime()) * BigInt(1_000_000)
+          ).toString();
+          originalResult.snapshot.block_height += "1";
+        }
+
+        return originalResult;
+      },
+    });
+    await page.goto(
+      "/infrastructure-committee.near/widget/near-prpsls-bos.components.pages.app?page=rfp&id=1"
+    );
+    await setDontAskAgainCacheValues({
+      page,
+      widgetSrc:
+        "infrastructure-committee.near/widget/near-prpsls-bos.components.rfps.Editor",
+      methodName: "edit_rfp",
+    });
+
+    await page.getByRole("button", { name: "Edit" }).click();
+
+    const descriptionArea = await page
+      .frameLocator("iframe")
+      .locator(".CodeMirror textarea");
+    await descriptionArea.fill(theNewDescription);
+    await descriptionArea.blur();
+
+    await pauseIfVideoRecording(page);
+    await page.getByRole("button", { name: "Submit" }).click();
+    const transactionToast = await page.locator(".toast-header");
+    await expect(transactionToast).toHaveText("Sending transaction");
+    await expect(transactionToast).not.toBeAttached();
+    const descriptionMarkdownElement = await page.locator(
+      "div[data-component='devhub.near/widget/devhub.components.molecule.MarkdownViewer']"
+    );
+    await descriptionMarkdownElement.scrollIntoViewIfNeeded();
+    await expect(descriptionMarkdownElement).toHaveText(theNewDescription);
     await pauseIfVideoRecording(page);
   });
 });
